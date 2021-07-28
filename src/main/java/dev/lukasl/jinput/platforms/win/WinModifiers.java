c4bf8e979048d1eec4d486408b7b7b901a31f8b4
lukasl-dev <git@lukasl.dev>
2021-07-28 21:24:22 +0200

    feat(windows): add WinModifiers

---
+++ src/main/java/dev/lukasl/jinput/platforms/win/WinModifiers.java
@@ -0,0 +1,28 @@
+package dev.lukasl.jinput.platforms.win;
+
+import dev.lukasl.jinput.hotkey.Modifier;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+final class WinModifiers {
+  private static final Map<Modifier, Integer> modifiers = new HashMap<Modifier, Integer>() {{
+    this.put(Modifier.CONTROL, 0x0002);
+    this.put(Modifier.COMMAND, 0x0008);
+    this.put(Modifier.SHIFT, 0x0004);
+    this.put(Modifier.OPTION, 0x0001);
+    this.put(Modifier.NO_REPEAT, 0x4000);
+  }};
+
+  static boolean supported(@NotNull Modifier modifier) {
+    return WinModifiers.modifiers.containsKey(modifier);
+  }
+
+  static int toInt(@NotNull Modifier modifier) {
+    if (!WinModifiers.supported(modifier)) {
+      throw new IllegalArgumentException();
+    }
+    return WinModifiers.modifiers.get(modifier);
+  }
+}
